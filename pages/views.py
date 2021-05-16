0c1b39da24647d2ceb233b7044d4c2f5343d2b31
parkash <parkashsatiyaar0008@gmail.com>
2021-05-16 13:46:46 +0530

    initial login setup

---
+++ pages/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'pages/home.html')
+
+
+def about(request):
+    return render(request, 'pages/home.html')
+
+
+def contact(request):
+    return render(request, 'pages/home.html')
